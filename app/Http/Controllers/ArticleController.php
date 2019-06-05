b2b5ed9a35254e7a22927ab7807e922b23d072cd
Phiricen <phiricen@gmail.com>
2019-06-05 13:30:49 +0900

    完成基本架構

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,18 +4,67 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use DateTime;
+use DatePeriod;
+use DateInterval;
 
 class ArticleController extends BaseController {
-    public function __construct(Request $request){
+	public function __construct(Request $request){
 
-    }
-    public function list(){
-        return view('article/list');
-    }
-    public function view($site_code, $article_id){
-        // $article_id = Route::current()->getParameter('article_id');
-        // $site_code = Route::current()->getParameter('site_code');
-        echo '/'.$site_code.'/'.$article_id;
-        return view('article/view');
-    }
+	}
+	public function list(){
+
+		$categories = [
+			'daily' => [
+				'title' => '每日焦點',
+				'code' => 'daily',
+				'dates' => $this->get_dates(),
+			],
+			'reference' => [
+				'title' => '來源網站',
+				'code' => 'reference',
+				'sites' => $this->get_sites(),
+			],
+		];
+
+		return view('article/list', [
+			'categories' => $categories,
+		]);
+		
+	}
+	public function view($site_code, $article_id){
+		// $article_id = Route::current()->getParameter('article_id');
+		// $site_code = Route::current()->getParameter('site_code');
+		echo '/'.$site_code.'/'.$article_id;
+		return view('article/view');
+	}
+
+	private function get_dates(){
+		$dates = [];
+		$date_string = date('Y-m-d');
+		$date_1 = new DateTime($date_string);
+		$date_2 = new DateTime($date_string);
+		$date_2->modify('-3 days');
+		$interval = new DateInterval('P1D');
+		$daterange = new DatePeriod($date_2, $interval ,$date_1);
+		foreach ($daterange as $date){
+			$dates[] = $date->format('Y-m-d');
+		}
+		$dates = array_reverse($dates);
+		return $dates;
+	}
+
+	private function get_sites(){
+		$sites = [
+			'dq_yam_com' => [
+				'code' => 'dq_yam_com',
+				'title' => '地球圖輯隊',
+			],
+			'cna_com_tw' => [
+				'code' => 'cna_com_tw',
+				'title' => '中央社',
+			],
+		];
+		return $sites;
+	}
 }
--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -1,14 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">
-        <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
-        
-        <title>文章一覽</title>
-    </head>
-    <body>
-    </body>
+		<link rel="stylesheet" href="{{ URL::asset('lib/bootstrap/bootstrap-4.3.1/dist/css/bootstrap.min.css') }}">
+		<script type="text/javascript" src="{{ URL::asset('lib/bootstrap/bootstrap-4.3.1/dist/js/bootstrap.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::asset('lib/jquery/jquery-3.4.1.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::asset('js/article.js') }}"></script>
+		
+		<title>文章一覽</title>
+	</head>
+	<body>
+		<div class="container mt-5 d-flex">
+			<div class="col-4 d-dlex">
+				<ul class="list-group">
+					<li class="list-group-item list-group-item-primary d-flex justify-content-between align-items-center">
+						<a>{{ $categories["daily"]["title"] }}</a>
+					</li>
+@foreach ($categories["daily"]["dates"] as $date)
+					<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+						<a>{{ $date }}</a>
+					</li>
+@endforeach
+					<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+						<a>所有文章</a>
+					</li>
+				</ul>
+				<ul class="list-group">
+					<li class="list-group-item list-group-item-primary d-flex justify-content-between align-items-center">
+						<a>{{ $categories["reference"]["title"] }}</a>
+					</li>
+@foreach ($categories["reference"]["sites"] as $site)
+					<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+						<a>{{ $site["title"] }}</a>
+						<span class="badge badge-primary badge-pill">0</span>
+					</li>
+@endforeach
+				</ul>
+			</div>
+		   <div class="col-8 d-flex card">
+		   </div>
+		</div>
+	</body>
 </html>
