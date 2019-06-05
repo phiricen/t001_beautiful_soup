f127b6fd22079ae1026b0ad5da0c2d4e587b4fa4
Phiricen <phiricen@gmail.com>
2019-06-05 10:05:39 +0900

    引用外部 CSS 及 JS

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">
+        <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
+        
         <title>文章一覽</title>
     </head>
     <body>
